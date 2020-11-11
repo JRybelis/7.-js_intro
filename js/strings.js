18ead27c44ea56c4bbfc24da0055c9cf8aa9ecd7
JRybelis <72306551+JRybelis@users.noreply.github.com>
2020-11-11 16:15:04 +0200

    teksto pvz

---
+++ js/strings.js
@@ -0,0 +1,20 @@
+const vardas2 = "Vardenis";
+const pasisveikinimas = ""
+console.log(vardas);
+
+// Mother said: "Don't do it!"
+const mother1 = "Mother said: \"Don't do it!\".";
+const mother2 = 'Mother said: "Don\'t do it!".';
+console.log(mother1);
+console.log(mother2);
+
+// <div> 
+    // <p> Lorem ipsum </p>
+    // <a href="#"> Click me</a>
+    // <div>
+
+const html = `<div>
+    <p> Lorem ipsum </p>
+    <a> href="#"> Click me </a>
+    </div>`;
+console.log(html);
